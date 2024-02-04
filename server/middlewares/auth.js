be66da68c0e1a5cdbe3f58c63438c7a112cbb78f
Shubham04Sanap <shubhamsanap0705@gmail.com>
2024-02-04 15:34:18 +0530

    role-based access added.

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const User = require("../models/user");
 
-exports.auth = async(req,res) =>{
+exports.auth = async(req, res, next) =>{
   try{
     const token  = req.cookies.token 
                    || req.body.token
@@ -34,4 +34,56 @@ exports.auth = async(req,res) =>{
   }
 }
 
-ex
+exports.isInstructor = async(req, res, next) =>{
+  try{
+    const role = req.user.accountType;
+
+    if(role !== "Student"){
+      res.status(401).json({
+        success:false,
+        message:"This is a protected route for student only!",
+      })
+    }
+  }catch(error){
+    res.status(501).json({
+      success:false,
+      message:"user cannot be verified",
+    })
+  }
+}
+
+exports.isInstructor = async(req, res, next) =>{
+  try{
+    const role = req.user.accountType;
+
+    if(role !== "Instructor"){
+      res.status(401).json({
+        success:false,
+        message:"This is a protected route for Instructor only!",
+      })
+    }
+  }catch(error){
+    res.status(501).json({
+      success:false,
+      message:"user cannot be verified",
+    })
+  }
+}
+
+exports.isAdmin = async(req, res, next) =>{
+  try{
+    const role = req.user.accountType;
+
+    if(role !== "Admin"){
+      res.status(401).json({
+        success:false,
+        message:"This is a protected route for Admin only!",
+      })
+    }
+  }catch(error){
+    res.status(501).json({
+      success:false,
+      message:"user cannot be verified",
+    })
+  }
+}
